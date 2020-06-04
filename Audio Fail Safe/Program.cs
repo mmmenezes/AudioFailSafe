222df31f8ee817090fc193bb3b8209890a5979bb
mmmenezes <53903641+mmmenezes@users.noreply.github.com>
2020-06-04 14:05:04 -0300

    Initial Commit

---
+++ Audio Fail Safe/Program.cs
@@ -0,0 +1,27 @@
+﻿using Topshelf;
+
+namespace Audio_Fail_Safe
+{
+    class Program
+    {
+        static void Main()
+        {
+            HostFactory.Run(configurator =>
+            {
+                configurator.Service<RestartService>(s =>
+                {
+
+                    s.ConstructUsing(name => new RestartService());
+
+                    s.WhenStarted((service, control) => service.Start(control));
+
+                    s.WhenStopped((service, control) => service.Stop(control));
+                });
+                configurator.RunAsLocalSystem();
+                configurator.SetDescription("Servico pra reiniciar audio");
+                configurator.SetDisplayName("ResetAudio");
+                configurator.SetServiceName("ResetAudio");
+            });
+        }
+    }
+}
---
+++ Audio Fail Safe/RestartService.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Configuration;
+using System.IO;
+using System.ServiceProcess;
+using System.Threading;
+using System.Threading.Tasks;
+using Topshelf;
+namespace Audio_Fail_Safe
+{
+    public class RestartService
+    {
+      
+        public bool Start(HostControl hostControl)
+        {
+
+            Task.Run(() =>
+            {
+                int Contador = 0;
+                
+                while (true)
+                {
+                    
+                    ServiceController service = new ServiceController("Windows Audio");
+                    if (service.Status == ServiceControllerStatus.Stopped)
+                    {
+                        Contador++;
+                            service.Start();
+                            using (var tw = new StreamWriter(ConfigurationManager.AppSettings.Get("Caminho"), true))
+                            {
+                                tw.WriteLine("Serviço Reiniciado Data:" + DateTime.Now.ToString()+ " Contador: " + Contador);
+                                tw.Close();
+                            }                      
+                    }
+                    service.Dispose();
+                    Thread.Sleep(TimeSpan.FromMinutes(1));
+                }
+            });
+
+            return true;
+
+        }
+
+        public bool Stop(HostControl hostControl)
+        {         
+            return true;
+        }
+    }
+}
